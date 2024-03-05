4f07b5a657763adccce0792019ae51a11c0e914f
gabriel <gabrielipcarvalho@gmail.com>
2024-03-05 20:22:34 +1000

    updated code for PSet1 problem3

--- 1 - Intro to CS/Intro + Python/PSet1/problem3.py
+++ 1 - Intro to CS/Intro + Python/PSet1/problem3.py
@@ -7,37 +7,32 @@
 # Longest substring in alphabetical order is: abc
 # Note: This problem may be challenging. We encourage you to work smart. If you've spent more than a few hours on this problem, we suggest that you move on to a different part of the course. If you have time, come back to this problem after you've had a break and cleared your head.
 
-# string += ch
-# loop:
-#   check ch index in the alphabet
-#   check ch[+1] index in the alphabet
-#   if ch[+1] index > ch index:
-#       string += ch[+1]
-    
- 
+#### OLD VERSION:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# string = ""
+# final_string = ""
+# index = -1
+# for c in s:
+#     for j, ch in enumerate(alphabet):
+#         if c == ch:
+#             if j >= index:
+#                 index = j
+#                 string += ch
+#                 if len(string) > len(final_string):
+#                     final_string = string
+#             else:    
+#                 index = j
+#                 string = ch
+# print("Longest substring in alphabetical order is:", final_string)
 
-s = "aababcabcdaaaazabcdefgdksjfhsdjkfh"
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+#### UPDATED VERSION:
 string = ""
 final_string = ""
-index = -1
-limit = len(s) - 1
-
-for c in s:
-    for j, ch in enumerate(alphabet):
-        if c == ch:
-            if j >= index:
-                index = j
-                string += ch
-                if len(string) > len(final_string):
-                    final_string = string
-            else:    
-                index = j
-                string = ch
+for i, c in enumerate(s):
+    if string == "" or c >= s[i-1]:
+        string += c
+        if len(string) > len(final_string):
+            final_string = string
+    else:
+        string = c
 print("Longest substring in alphabetical order is:", final_string)
-        # for k, char in enumerate(alphabet):
-        #     if char == s[i+1]:
-        #         indexplus = k
-        # if indexplus > index:
-        #     string += c
-        #     string += s[i+1] 
